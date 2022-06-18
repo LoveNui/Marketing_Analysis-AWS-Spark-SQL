d4a18611865f959d81dcac6372b844b1f90705a0
LoveNui <yujinxian1212@gmail.com>
2022-06-17 23:41:16 -0400

    Completed deliverable 1

---
+++ challenge_schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE review_id_table (
+  review_id TEXT PRIMARY KEY NOT NULL,
+  customer_id INTEGER,
+  product_id TEXT,
+  product_parent INTEGER,
+  review_date DATE -- this should be in the formate yyyy-mm-dd
+);
+
+-- This table will contain only unique values
+CREATE TABLE products_table (
+  product_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+  product_title TEXT
+);
+
+-- Customer table for first data set
+CREATE TABLE customers_table (
+  customer_id INT PRIMARY KEY NOT NULL UNIQUE,
+  customer_count INT
+);
+
+-- vine table
+CREATE TABLE vine_table (
+  review_id TEXT PRIMARY KEY,
+  star_rating INTEGER,
+  helpful_votes INTEGER,
+  total_votes INTEGER,
+  vine TEXT,
+  verified_purchase TEXT
+);
